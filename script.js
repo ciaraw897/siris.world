5e4b63677aea08c3c666bcf311b0ebf58a817461
Ciara Wolfe <ciaraw897@gmail.com>
2021-12-10 19:44:49 +0000

    added about page

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
+//RANDOM NUMBER BUTTON
 function buttonClick() {
   return Math.floor(Math.random()*1000);
 }
 
+//CURRENT DATE
+var today = new Date();
+var day = today.getDay();
+var daylist = ["Sunday","Monday","Tuesday","Wednesday ","Thursday","Friday","Saturday"];
+var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+var dateTime = date+' '+time;
+ 
+document.getElementById("displayDateTime").innerHTML = dateTime + ' <br> Day :- ' + daylist[day];
+
+
+//CONFETTI
 //-----------Var Inits--------------
 const canvas = document.getElementById("canvas");
 ctx = canvas.getContext("2d");
